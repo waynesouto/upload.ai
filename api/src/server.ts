e7b49e450b3e2138a0305f532cef725fa34aa67e
Wayne Souto <soutowhs@gmail.com>
2023-09-12 23:39:28 -0300

    api initial commit

---
+++ api/src/server.ts
@@ -0,0 +1,23 @@
+import { fastifyCors } from "@fastify/cors";
+import { fastify } from "fastify";
+import { createTranscriptionRoute } from "./routes/create-transcription";
+import { generateAiCompletionRoute } from "./routes/generate-ai-completion";
+import { getAllPromptsRoute } from "./routes/get-all-prompts";
+import { uploadVideoRoute } from "./routes/upload-video";
+
+const app = fastify();
+
+app.register(fastifyCors, {
+  origin: "*",
+});
+
+app.register(getAllPromptsRoute);
+app.register(uploadVideoRoute);
+app.register(createTranscriptionRoute);
+app.register(generateAiCompletionRoute);
+
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => console.log("Server started at http://localhost:3333/"));
